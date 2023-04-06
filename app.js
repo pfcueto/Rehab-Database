9a0914ce36d43e3e6400f42bf55bd338d57aa4ea
Pitter Cueto <pittercueto@gmail.com>
2023-04-06 21:10:22 +0200

    Changed app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Create a date
+const now = new Date();
+console.log(now);
+console.log(new Date("Aug 02 2020 18:05:42"));
+console.log(new Date("December 25,2020"));
+console.log(new Date(account1.movements[0]));
+console.log(new Date(2018, 10, 10, 14, 29, 39));
+console.log(new Date(0));
